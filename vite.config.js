208f3a70a54f104809fc7eee7af999af92cde0a1
Muhammad Rafay <muhammadrafay1k@gmail.comm>
2026-09-24 16:38:19 +0500

    fix: install and include tailwindcss vite plugin

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: '/',
+  plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Increases the warning threshold to 1000 kB
+  },
 })
